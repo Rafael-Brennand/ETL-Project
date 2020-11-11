dc5be8d6a9b43e597c8f15166fb9c75ed5e62c3c
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2020-11-10 21:15:40 -0800

    added query to sql file

--- queries.sql
+++ queries.sql
@@ -15,3 +15,12 @@ IMDB_Rating DECIMAL,
 Metacritic_Rating INT,
 Awards VARCHAR
 );
+
+SELECT *
+FROM movie_ratings
+INNER JOIN netflix_movies ON
+netflix_movies.title = movie_ratings.Movie_Title;
+
+
+
+-- netflix_movies.title, netflix_movies.country, netflix_movies.release_year, netflix_movies.rating, movie_ratings.Movie_Title , movie_ratings.IMDB_Rating,movie_ratings.Metacritic_Rating,movie_ratings.Awards
